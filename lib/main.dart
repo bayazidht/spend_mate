3c66b03b9a3476e23e0d708fc77eeb8dba4fb877
Syed Bayazid Hossain <44992194+bayazidht@users.noreply.github.com>
2025-11-26 17:40:14 +0900

    Refactor imports and update provider initialization

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:spend_mate/services/auth_service.dart';
-import 'package:spend_mate/providers/transaction_provider.dart'; // নতুন import
+import 'package:spend_mate/providers/transaction_provider.dart'; 
 import 'package:spend_mate/screens/wrapper.dart';
 import 'firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart'; // নতুন import
+import 'package:firebase_auth/firebase_auth.dart'; 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,28 +22,23 @@ class SpendMateApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        // 1. AuthService (Provides User stream)
         Provider<AuthService>(
           create: (_) => AuthService(),
         ),
 
-        // 2. StreamProvider for User (Handles logged-in state)
         StreamProvider<User?>.value(
           value: AuthService().user,
           initialData: null,
           catchError: (_, __) => null,
         ),
 
-        // 3. ChangeNotifierProxyProvider (Depends on the User stream)
-        // এটি নিশ্চিত করে যে User যখন লগ ইন করবে তখনই TransactionProvider তৈরি হবে
+       
         ChangeNotifierProxyProvider<User?, TransactionProvider>(
-          create: (context) => TransactionProvider(null), // Initial provider with null user
+          create: (context) => TransactionProvider(null), 
           update: (context, user, previousProvider) {
-            // Re-create the provider only when the user status changes
             if (user != null) {
               return TransactionProvider(user);
             }
-            // If user logs out, keep the previous provider or return a null-user version
             return TransactionProvider(null);
           },
           lazy: false,
